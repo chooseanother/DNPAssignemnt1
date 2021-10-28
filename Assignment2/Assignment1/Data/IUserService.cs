2e50bb1198d8cd3f48a632d6915ee5d303059477
Kim Tranberg <kim.tranberg@gmail.com>
2021-10-28 21:03:51 +0200

    fix

--- Assignment2/Assignment1/Data/IUserService.cs
+++
@@ -1,7 +0,0 @@
-using Models;
-
-namespace Data {
-public interface IUserService {
-    User ValidateUser(string userName, string password);
-}
-}
--- Assignment2/Assignment1/Models/User.cs
+++
@@ -1,6 +0,0 @@
-namespace Models {
-public class User {
-    public string UserName { get; set; }
-    public string Password { get; set; }
-}
-}
